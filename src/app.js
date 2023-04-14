16192692f1e86ea61224f414c7f9ab27607c6a09
pavildon <monomorphism@protonmail.com>
2023-04-14 16:12:23 +0100

    yoga

--- src/app.js
+++ src/app.js
@@ -1,33 +1,23 @@
-import path from 'path'
-import express from 'express'
 
 
-const __dirname = path.resolve()
+import { createServer } from "http"
+import { createSchema, createYoga } from "graphql-yoga"
 
-const app = express()
-const port = 3000
-
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: (res) => {
-    res.set('x-timestamp', Date.now())
-  }
-}
-
-
-app.use('/', express.static(__dirname + '/static'))
-
-
-app.get('/api/', async (_req, res) => {
-  res.json("ok")
+createServer(
+  createYoga({
+    schema: createSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          hello: String
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => "Hello Hello Hello",
+        },
+      },
+    }),
+  })
+).listen(4000, () => {
+  console.info("GraphQL Yoga is listening on http://localhost:4000/graphql")
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
