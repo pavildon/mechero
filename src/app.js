2a46bc0e9e13734a9b2e097fa878157e008c9cb2
pavildon <monomorphism@protonmail.com>
2023-04-14 16:00:34 +0100

    2nd

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import express from 'express'
+
+
+const __dirname = path.resolve()
+
+const app = express()
+const port = 3000
+
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: (res) => {
+    res.set('x-timestamp', Date.now())
+  }
+}
+
+
+app.use('/', express.static(__dirname + '/static'))
+
+
+app.get('/new_driver/', async (_req, res) => {
+  res.json("ok")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
